fe15fbfc3f4a7a1f5d1fdf62a2f651d8085df231
RussellShire <russell.shire@gmail.com>
2022-07-13 13:30:26 +0100

    Using event listeners to record input and edit DOM

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 let userName = 'Russell';
-const userQuestion = 'Will I win the lottery?';
+let userQuestion = 'Will I win the lottery?';
 
-const eightBallAnswers = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes']
+// Arrays of text for 8 Ball to say
+const eightBallAnswers = ['It is certain', 'You already know this', 'I sincererly hope not', 'Unfortunately, yes', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes']
+const eightBallThinking = ['I shall think on this', 'Patenice while I predict your pathetic future', `I knew you would ask that, classic ${userName}`, 'How unimaginative', 'A question barely worth my talents, but money is money', 'A most intriguing question. Makes a change', 'How, novel, I shall investigate fate on your behalf']
 
-const eightBallThinking = ['I shall think on this', 'Patenice while I predict your pathetic future', `I knew you would ask that, classic ${userName}`, 'How unimaginative', 'A question barely worth my talents, but money is money', 'A most intrieging question. Makes a change', 'How, novel, I shall investigate fate on your behalf']
-
-
-const randArrayElement = (array) => {
+// A function that takes an array and returns a random number between zero and the length of an array-1
+const getRandArrayIndex = (array) => {
   return Math.floor(Math.random()*(array.length-1));
   }
 
-const randArrayLog = (array) => {
-  return console.log(array[randArrayElement(array)]);
+// A function that takes the results of getRandArrayIndex and prints the corresponding array element
+const logRandArrayElem = (array) => {
+  return console.log(array[getRandArrayIndex(array)]);
   }
 
-userName != '' ? console.log(`Welcome ${userName}, what brings you before the fateweaver?`) : console.log('Welcome');
 
-console.log(`You're asking the almighty all-knowing magic 8 Ball '${userQuestion.replace(' I ', ` ${userName} `)}'`)
 
-randArrayLog(eightBallThinking)
-randArrayLog(eightBallAnswers)
+//userName != '' ? console.log(`Welcome ${userName}, what brings you before the fateweaver?`) : console.log('Welcome');
+
+//console.log(`You're asking the almighty all-knowing magic 8 Ball '${userQuestion.replace(' I ', ` ${userName} `)}'`)
+
+//logRandArrayElem(eightBallThinking);
+//logRandArrayElem(eightBallAnswers);
+
+const button = document.getElementById('button1');
+const inputBox = document.getElementById('input');
+let userInput = '';
+const eightBallSpeak = document.querySelector('.edit-text')
+
+// Assigns the input value to userInput when button is pressed, then clears the input box
+button.addEventListener('click', function(userInput){
+  userInput = inputBox.value
+  inputBox.value = ''; // clears the box 
+  speaking(`hello ${userInput}`)
+});
+
+
+function speaking(text) {
+  eightBallSpeak.textContent = text;
+}
