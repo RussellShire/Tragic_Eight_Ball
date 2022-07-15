cf88481f9f1c60ee8bcac46717ff70ffedcf815d
RussellShire <russell.shire@gmail.com>
2022-07-15 12:59:17 +0100

    Refactored to use localStorage

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
 // All my variables
-let inputArray = ['', ''];
+let inputArray = [''];
 let userName = inputArray[0];
 let userCounter = 0;
 let userInput = inputArray[userCounter];
 
 const button = document.getElementById('button1');
 const inputBox = document.getElementById('input');
-const eightBallSpeak = document.querySelector('.edit-text');
+const eightBallSpeak = document.getElementById('edit-text-id');
 
 // Arrays of text for 8 Ball to say
 const eightBallAnswers = ['It is certain', 'You already know this', 'I sincererly hope not', 'The answer is obvious', 'Unfortunately, yes', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes']
 const eightBallThinking = ['I shall think on this', 'Patenice while I predict your pathetic future', `I knew you would ask that, classic ${userName}`, 'How unimaginative', 'A question barely worth my talents, but money is money', 'A most intriguing question. Makes a change', 'How, novel, I shall investigate fate on your behalf']
-const eightBallWelcome = [`Welcome ${inputArray[0]} what knowledge do you seek?`]
-const eightBallGreeting = ['Who comes before me?', 'Who disturbs the allknowing?', 'Who are you that distrubs my slumber?', 'Another petitioner for my predictive powers, state your name mortal.', 'State your name and I shall consider a request.']
-
-console.log(eightBallSpeak);
-const welcome = () => {
-// E-xtract
-  const inputBox = document.getElementById('input');
-  const eightBallSpeak = document.getElementById('edit-text-id');
-// T-ransform
-// trying to deep copy an element which does not work
-//  values = ['']
-//  values[0] += inputBox.value
-//  console.log(values[0])
-// L-oad
-  eightBallSpeak.innerText = 'Welcome ' + inputBox.value + ' what knowledge do you seek?';
-  inputBox.value = ''; //this wipes the element later 
-};
-
+const eightBallWelcome = [`Welcome ${localStorage.getItem('userName')}`];
+const eightBallGreeting = ['Who comes before me?', 'Who disturbs the allknowing?', 'Who are you that disturbs my slumber?', 'Another petitioner for my predictive powers, state your name mortal.', 'State your name and I shall consider a request.']
 // an array of the arrays of text - so the counter can cycle through them
 const speakingArray = [eightBallGreeting, eightBallWelcome,eightBallAnswers];
 
@@ -48,22 +32,21 @@ If user counter is 2 then it records the input and doesn't move the counter forw
 This goes to 3 because there are currently 3 phases of speech in the speakingArray
 This is my homebrew solution for using the same click on a button to do multiple things
 */
-//QUESTION FOR GRAHAM - Why isn't name being assigned? I think it's a scope issue?
-
-const inputSave = function() { // creating this as a function so it can be resued in the else statment below
-  //inputBox.value = '' // clears the input box
-  speakTest = speakingArray[userCounter]
-  speaking(logRandArrayElem(speakTest))};
 
 const run = function() {
-  inputArray[userCounter] = inputBox.value //adds the input to the inputArray at the position of userCounter
-    if(userCounter <= 1) { //if statement allows the first two questions to be saved, then stops the counter
+  if(userCounter <= 1) { //if statement allows the first two questions to be saved, then stops the counter
         userCounter ++
         //welcome()
         inputSave();
       } else {
         inputSave();
       }};
+      
+const inputSave = function() { // creating this as a function so it can be resued in the else statment below
+  localStorage.setItem('userName', inputBox.value); //adds the input to the inputArray at the position of userCounter
+  console.log(localStorage.getItem('userName'))
+  inputBox.value = '' // clears the input box
+  speaking(logRandArrayElem(speakingArray[userCounter]))};
 
 //QUESTION FOR GRAHAM: This is technically the start of my code. Where is best practice for it to be?
 /*kicking off with a nested function
@@ -73,26 +56,11 @@ speaking(logRandArrayElem(eightBallGreeting));
 
 //QUESTION FOR GRAHAM: Can I combine two event listeners that trigger the same event?
 // EVENT LISTENERS - two, one for button click and one for keypress Enter, both run big function run 
-// button.addEventListener('click', () => {
-//   run();  
-//   });
-
- inputBox.addEventListener('keypress', function(e) {
-   if (e.key === 'Enter') { 
-     run()
-     console.log(inputArray);
-
-     }});
-
-//test
-let CLICKS = 0;
-
 button.addEventListener('click', () => {
-  welcome();  
-  if (CLICKS === 0) {
-    welcome();
-    CLICKS += 1;  
-  } else {
-    console.log('else has happened')
-  }
-});
+   run();  
+   });
+
+inputBox.addEventListener('keypress', function(e) {
+  if (e.key === 'Enter') { 
+    run()     
+  }});
