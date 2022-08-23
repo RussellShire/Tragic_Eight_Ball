a230859f4dc77c89471e75ec3ef44947a0088e16
RussellShire <russell.shire@gmail.com>
2022-08-23 11:56:26 +0100

    Add basic wiring from model to view via controller

--- src/mvc/controller.js
+++ src/mvc/controller.js
@@ -1,6 +1,6 @@
 export default class Controller {
     constructor(model, view) {
-        this.model = model
+        this.eightBall = model
         this.view = view
     }
 
@@ -14,34 +14,40 @@ export default class Controller {
         console.log(this.view.hello())
     }
 
-    mediator(userInput) {
-        let state = 'welcome'
+    eightballSpeaking(){
+        const speech = this.eightBall.speak()
+        const controller = this
 
-        switch (state){
-            case 'welcome':
-                // Call model functions with userInput
-                state = 'greeting'
-                break;
-            case 'greeting':
-                // Call model functions with userInput
-                state = 'inviteQuestion'
-                break;
-            case 'inviteQuestion':
-                // Call model functions with userInput
-                state = 'thinking'
-                break;
-            case 'thinking':
-                // Call model functions with userInput
-                state = 'answer'
-                break;
-            case 'answer':
-                // Call model functions with userInput
-                state = 'inviteQuestion'
-                break;    
-            }
-        
-            const controller = this;
-        this.view.render(controller, mediatorResponse) // taking output from the model + sending controller through
-    
+        this.view.render(controller, speech)
     }
+
+    // mediator(userInput) {
+    //     let state = 'welcome'
+
+    //     switch (state){
+    //         case 'welcome':
+    //             // Call model functions with userInput
+    //             state = 'greeting'
+    //             break;
+    //         case 'greeting':
+    //             // Call model functions with userInput
+    //             state = 'inviteQuestion'
+    //             break;
+    //         case 'inviteQuestion':
+    //             // Call model functions with userInput
+    //             state = 'thinking'
+    //             break;
+    //         case 'thinking':
+    //             // Call model functions with userInput
+    //             state = 'answer'
+    //             break;
+    //         case 'answer':
+    //             // Call model functions with userInput
+    //             state = 'inviteQuestion'
+    //             break;    
+    //         }
+        
+    //     const controller = this;
+    //     this.view.render(controller, mediatorResponse) // taking output from the model + sending controller through
+    // }
 }
