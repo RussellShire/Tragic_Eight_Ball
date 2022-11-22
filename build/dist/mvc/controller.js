bcd5cd7ff0696dc2aa04ab759c667553efa51e80
RussellShire <russell.shire@gmail.com>
2022-11-22 10:27:05 +0000

    Build to gh-pages

---
+++ build/dist/mvc/controller.js
@@ -0,0 +1,19 @@
+export default class Controller {
+  constructor(model, view) {
+    this.eightBall = model;
+    this.view = view;
+    this.controller = this;
+  }
+
+  eightBallSpeaking() {
+    const speech = this.eightBall.speak();
+
+    this.view.render(this.controller, speech);
+  }
+
+  eightBallListening(response) {
+    this.eightBall.ponder(response);
+
+    this.eightBallSpeaking();
+  }
+}
